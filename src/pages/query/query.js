7bdd08b390cb59242b088fbd02922652fc49df6c
gunal15 <gunalnandakumar@gmail.com>
2023-02-27 15:48:17 +0530

    buton

--- src/pages/query/query.js
+++ src/pages/query/query.js
@@ -1,86 +1,80 @@
-/* eslint-disable react-hooks/exhaustive-deps */
-import {  Button, Container } from "react-bootstrap";
-import { useContext,useState,useEffect } from "react";
-import { DataContext } from "../../context/context";
-import "./query.css"
-import { FcSearch } from "react-icons/fc";
-import { AiFillThunderbolt } from "react-icons/ai";
-import DisplayCard from "../../components/card/card";
-import { Link } from "react-router-dom";
-
-export default function Query()
-{
-    const {data,mainTags}=useContext(DataContext)
-    const [filterData,setFilterData] =useState([])
-    const [search,setSearch]=useState()
-    const [tags,setTags]=useState([])
-
+export default function Query() {
+    const { data, mainTags } = useContext(DataContext);
+    const [filterData, setFilterData] = useState([]);
+    const [search, setSearch] = useState();
+    const [tags, setTags] = useState([]);
+    const [filterApplied, setFilterApplied] = useState(false);
   
-
-    useEffect(()=>{
-        // console.log("in mount")
-        setFilterData([...data])
-        // console.log(mainTags)
-        for (let key in mainTags)
-        {
-            // console.log(mainTags[key])
-           if( Object.keys(mainTags[key]).length>0)
-           {
-               mainTags[key].forEach((a)=>{
-                //    console.log(a)
-                   setTags(tags=>[...tags,a])
-               })
-           }
-        //    console.log(tags)
+    useEffect(() => {
+      setFilterData([...data]);
+      for (let key in mainTags) {
+        if (Object.keys(mainTags[key]).length > 0) {
+          mainTags[key].forEach((a) => {
+            setTags((tags) => [...tags, a]);
+          });
         }
-
-    },[])
-
-    if(data.length<1) return[]
-
-    const handleSearch=({target:{value}})=>{
-        setSearch(value)
-
-        if(value)
-        {
-            const regex = new RegExp(value,"gi")
-
-            let newData= filterData.filter((a)=>{
-                return regex.test(a.name)
-            }
-            )
-            console.log(newData)
-            setFilterData([...newData])
-        }
-        else{
-            setFilterData([...data])
-        }
-    }
-
-    return(
-        <>
+      }
+    }, []);
+  
+    if (data.length < 1) return [];
+  
+    const handleSearch = ({target:{value}}) => {
+      setSearch(value);
+  
+      if (value) {
+        const regex = new RegExp(value, "gi");
+  
+        let newData = filterData.filter((a) => {
+          return regex.test(a.name);
+        });
+        console.log(newData);
+        setFilterData([...newData]);
+        setFilterApplied(true);
+      } else {
+        setFilterData([...data]);
+        setFilterApplied(false);
+      }
+    };
+  
+    return (
+      <>
         <div className="outerContainer">
-        <Container className=" bg-muted d-flex flex-column align-items-center ">
-            {/* <div className="mainHeader "><h4><b><FcSearch/>The Best Way to Browse Quality AWS GitHub Repositories<FcSearch/></b></h4></div>
-            <div><p><AiFillThunderbolt style={{color:"yellow"}}/>Built with a spreadsheet + <a  href="https://www.polymersearch.com/?utm_source=built_with_polymer_aws" target="_blank" rel="noreferrer"   >Polymer Search</a>|<a href="https://app.polymersearch.com/auth/login" target="_blank" rel="noreferrer"> Create a search and data app in seconds with Polymer beta</a></p></div> */}
-            <p style={{textAlign:"center",fontFamily:"Consolas",fontWeight:"bolder",color:"black",fontSize:"30px", marginTop:"20px", marginBottom:"20px"}}>aws_browser.csv</p>
+          <Container className=" bg-muted d-flex flex-column align-items-center ">
+            <p
+              style={{
+                textAlign: "center",
+                fontFamily: "Consolas",
+                fontWeight: "bolder",
+                color: "black",
+                fontSize: "30px",
+                marginTop: "20px",
+                marginBottom: "20px",
+              }}
+            >
+              aws_browser.csv
+            </p>
             <div className="searchContainer">
-                <div>
-                    <form>
-                        <input  className="searchBox form-control" placeholder="search..." onChange={handleSearch} value={search}/>
-                    </form>
-                </div>
+              <div>
+                <form>
+                  <input
+                    className="searchBox form-control"
+                    placeholder="search..."
+                    onChange={handleSearch}
+                    value={search}
+                  />
+                </form>
+              </div>
             </div>
-        </Container>
-        <div className="tagBar">
+          </Container>
+          <div className="tagBar">
             <div className="ms-5 mt-2">
-                <p >TAGS :
-                    {
-                    tags.map((a)=>{
-                        return <div className="tagBarItem text-muted">{a}</div>
-                    })
-                    }
-                    <Button className="clearbutton"><a href="https://eclectic-haupia-0bab18.netlify.app/">Clear</a></Button>
+              <p>
+                TAGS :
+                {tags.map((a) => {
+                  return <div className="tagBarItem text-muted">{a}</div>;
+                })}
+                <Button className="clearbutton">
+                  <a href="https://eclectic-haupia-0bab18.netlify.app/">Clear</a></Button>
                 </p>
             </div>
         </div>
